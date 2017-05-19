1aaa2034020297fe78c2a0e12b713b835e13a156
Sergio Junior <sergiojcamposjr@gmail.com>
2017-05-19 15:35:56 -0300

    Change redis url and redis port

--- thumborizeme/settings.py
+++ thumborizeme/settings.py
@@ -1,8 +1,8 @@
 import os
 
 # REDIS
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
+REDIS_HOST = os.environ.get('REDIS_URL', '127.0.0.1')
+REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 
 HOST = os.environ.get('HOST', 'http://localhost:9000')
 THUMBOR_HOST = os.environ.get('HOST', 'http://localhost:8001')
