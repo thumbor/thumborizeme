0cddd9ba56a6b1160c7ca473bb9907f01197b5cc
Sergio Junior <sergiojcamposjr@gmail.com>
2017-05-19 17:20:16 -0300

    Change redis_host enviroment variable

--- thumborizeme/settings.py
+++ thumborizeme/settings.py
@@ -1,7 +1,7 @@
 import os
 
 # REDIS
-REDIS_HOST = os.environ.get('REDIS_URL', '127.0.0.1')
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
 
