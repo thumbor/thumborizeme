015ba85a83b85bf09e1dc6ea19922406ea5cf276
Raphael Vieira Rossi <raphael.rossi@g.globo>
2022-10-17 16:21:49 -0300

    Change comment line

--- infra/modules/gce/gce.tf
+++ infra/modules/gce/gce.tf
@@ -18,6 +18,13 @@ resource "google_compute_instance" "thumborizeme" {
 
     access_config {
       // Ephemeral public IP
+      // https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#access_config
+      // (Optional) Access configurations, i.e. IPs via which this instance can
+      // be accessed via the Internet. Omit to ensure that the instance is not
+      // accessible from the Internet. If omitted, ssh provisioners will not
+      // work unless Terraform can send traffic to the instance's network
+      // (e.g. via tunnel or because it is running on another cloud instance on
+      // that network).
     }
   }
 
